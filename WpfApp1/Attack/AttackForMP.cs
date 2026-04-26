cda6c05abec1ea4219577e1ec621232eff41a096
Hospitaler <deadman4989@gmail.com>
2026-04-26 21:25:46 +0900

    試しにファイルの移動を行いました。

--- WpfApp1/Attack/AttackForMP.cs
+++ WpfApp1/Attack/AttackForMP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp1.Characters;
 
 namespace WpfApp1.Attack
 {
--- WpfApp1/Effects/Effect.cs
+++ WpfApp1/Effects/Effect.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using WpfApp1.Characters;
 
 namespace WpfApp1
 {
